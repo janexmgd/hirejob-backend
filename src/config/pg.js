4d4ac3677e4cdf33e381ff0670589f3d6023b959
Denny Wahyu <janexmgd@gmail.com>
2022-06-15 12:37:41 +0700

    ADD MESSAGE WHEN DB CONNECTED

--- src/config/pg.js
+++ src/config/pg.js
@@ -1,29 +1,31 @@
 const { Pool } = require("pg");
 const {
-  PG_HOST,
-  PG_USER,
-  PG_PASSWORD,
-  PG_DATABASE,
-  PG_PORT,
+	PG_HOST,
+	PG_USER,
+	PG_PASSWORD,
+	PG_DATABASE,
+	PG_PORT,
 } = require("../helpers/env");
 
 const db = new Pool({
-  host: PG_HOST,
-  user: PG_USER,
-  password: PG_PASSWORD,
-  database: PG_DATABASE,
-  port: PG_PORT,
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
+	host: PG_HOST,
+	user: PG_USER,
+	password: PG_PASSWORD,
+	database: PG_DATABASE,
+	port: PG_PORT,
+	// ssl: {
+	//   rejectUnauthorized: false,
+	// },
 });
 
 // check connection
 db.connect((err) => {
-  if (err) {
-    console.log("db error");
-    console.log(err.message);
-  }
+	if (err) {
+		console.log("db error");
+		console.log(err.message);
+	} else {
+		console.log("DB CONNECTED");
+	}
 });
 
 module.exports = db;
